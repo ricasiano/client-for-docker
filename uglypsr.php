ae8c50a18640d9457d1d6de4ff32dee9f11c18fb
rai <ricasiano@gmail.com>
2021-05-28 23:21:56 +0800

    align with psr

--- uglypsr.php
+++ uglypsr.php
@@ -1,11 +1,13 @@
 <?php
 /**
- * @file
  * Description of what this module (or file) is doing.
+ * PHP VERSION 7.1
+ * @file
+ * @category  file
  *  @package  MyPackage
  *   @author  Display Name <who@are.you>
  *  @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
  *     @link     http://www.hashbangcode.com/
- * PHP VERSION 7.1
  */
+
 echo 1;
