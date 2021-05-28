112fa5e3985cf83fe2c8eb437cbac7c5fde04cd3
rai <ricasiano@gmail.com>
2021-05-28 23:05:03 +0800

    align with psr

--- uglypsr.php
+++ uglypsr.php
@@ -1,14 +1,9 @@
 <?php
 /**
+ * @file
  * MyClass Class Doc Comment
- *
- * @category Class
- * @package  MyPackage
- * @author    A N Other
- * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @link     http://www.hashbangcode.com/
  * @version  7.1
- *
  */
-class MyClass{
+class MyClass
+{
 }
