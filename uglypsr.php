f556bc52f1d3143339846f61399f73ba3dea7d63
rai <ricasiano@gmail.com>
2021-05-28 23:01:00 +0800

    align with psr

--- uglypsr.php
+++ uglypsr.php
@@ -1,8 +1,29 @@
 <?php
+/**
+ * @file
+ * Description of what this module (or file) is doing.
+ */
 $a = 1;
 if (1 === $a) {
     echo 2;
 }
+
+/**
+ * Class A
+ */
+class A
+{
+    /**
+     * @return int
+     */
+    public function noVisibility(): int
+    {
+        return 1;
+    }
+}
+
+$classA = new A();
+$classA->noVisibility();
 
 
 
